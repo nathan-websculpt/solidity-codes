d3720c10488623aa1dc4baf20eb56b8872d903be
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-24 09:58:14 -0500

    simple contract factory

--- _devving/main.sol
+++ _devving/main.sol
@@ -1,20 +1,30 @@
 pragma solidity >=0.8.0 <0.9.0;
 
-contract Main {
-    address public contractA;
-
-    constructor() public {
-        contractA = address(0);
+contract A {
+    struct Deployment {
+        address bAddr;
+        uint256 index;
+        string title;
     }
 
-    function setContractA(address _contractA) public {
-        require(contractA == address(0), "Contract A already set");
-        contractA = _contractA;
-    }
+    Deployment[] public deployments;
 
-    modifier onlyContractA() {
-        require(msg.sender == contractA, "Only Contract A can call this function");
-        _;
+    event NewB(address indexed b, uint256 indexed index, string title);
+
+    function deployB(uint256 index, string memory title) external {
+        B b = new B(index, title);
+        deployments.push(Deployment(address(b), index, title));
+        
+        emit NewB(address(b), index, title);
     }
 }
 
+contract B {
+    uint256 public index;
+    string public title;
+
+    constructor(uint256 _index, string memory _title) {
+        index = _index;
+        title = _title;
+    }
+}
